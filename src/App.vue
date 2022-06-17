47ed6e29e638871410956161e0ed6fe1ebd30f91
walofz <paitoon.j@outlook.co.th>
2022-06-17 17:00:44 +0700

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+</script>
+
+<template>
+  <section>
+    <h1>Doyalin G</h1>
+  </section>
+</template>
+
+<style>
+@import './assets/base.css';
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+
+@media (min-width: 1024px) {
+  body {
+    display: flex;
+    place-items: center;
+  }
+
+  #app {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    padding: 0 2rem;
+  }
+}
+</style>
